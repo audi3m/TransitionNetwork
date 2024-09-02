32505186b26f0a283a531ee3f88ab6368a2bd79d
ray <audi3m@naver.com>
2024-09-02 09:47:03 +0900

    Initial Commit

---
+++ TransitionNetwork/TransitionNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionNetworkApp.swift
+//  TransitionNetwork
+//
+//  Created by J Oh on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TransitionNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
